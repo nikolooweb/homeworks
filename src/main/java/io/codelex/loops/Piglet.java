8c52c160103d9c69abab1aca6ddef9a812d9c7ce
Nikolajs <nikolajs.rubaniks@inbox.lv>
2023-05-15 14:00:43 +0300

    updated piglet file

--- src/main/java/io/codelex/loops/Piglet.java
+++ src/main/java/io/codelex/loops/Piglet.java
@@ -6,11 +6,13 @@ import java.util.Scanner;
 public class Piglet {
     public static void main(String[] arg) {
 
-
         System.out.println("Welcome to Piglet!");
 
         boolean gameOn = true;
         int points = 0;
+        String[] commandsToContinue = {"yes", "y"};
+        String[] commandsToQuit = {"no", "n", "exit"};
+
         Scanner in = new Scanner(System.in);
 
         while (gameOn) {
@@ -20,9 +22,6 @@ public class Piglet {
 
             if (randomDice != 1) {
                 points += randomDice;
-
-                String[] commandsToContinue = {"yes", "y"};
-                String[] commandsToQuit = {"no", "n", "exit"};
 
                 System.out.println("Roll again?");
 
@@ -45,9 +44,6 @@ public class Piglet {
                     System.out.println("good job!!!");
                 } else if (negativeAnswer) {
                     System.out.printf("You got %d points.", points);
-                    gameOn = false;
-                } else {
-                    System.out.println("Invalid command... game over");
                     gameOn = false;
                 }
             } else {
